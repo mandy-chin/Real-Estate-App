4206fabc3cbf89a1ccd7ba6915a5d8d342824e1f
mandy-chin <chin.mandyy@gmail.com>
2023-01-09 20:30:54 -0500

    finished js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ addButton.addEventListener("click", () => {
     property: propertyType,
     city: city,
     state: state,
+    comment: [],
   });
 
   // get section where properties will appear
@@ -31,21 +32,57 @@ addButton.addEventListener("click", () => {
   // propertyElArray is new array every time new property is added;
   // .map() takes each element in allProperties array,
   const propertyElArray = allProperties.map((property) => {
-    // create section to hold property details and delete button
+    // create section to hold property details, comment button, and delete button
     const propertyElement = document.createElement("section");
+
+    const commentButton = document.createElement("button");
+    commentButton.textContent = "Comment";
+
+    // add event listener to comment button
+    commentButton.addEventListener("click", () => {
+      // create text box and send button
+      const commentText = document.createElement("input");
+      const sendButton = document.createElement("button");
+      sendButton.textContent = "Send";
+
+      // add event listener to send button
+      // when button is clicked, add comment to object in array
+      // generate DOM element on page
+      sendButton.addEventListener("click", () => {
+        if (commentText.value.length >= 1 && commentText.value.length <= 280) {
+          property.comment.push(commentText.value);
+
+          // create container for text
+          const comment = document.createElement("section");
+          // add text to container
+          comment.textContent = commentText.value;
+          // append to propertyElement
+          propertyElement.append(comment);
+        }
+      });
+
+      // append to propertyElement;
+      propertyElement.append(commentText, sendButton);
+    });
 
     const delButton = document.createElement("button");
     delButton.textContent = "Remove Property";
 
     // add event listener to delete button;
-    delButton.addEventListener("click", () => {});
+    delButton.addEventListener("click", () => {
+      propertyElement.remove();
+      const indexOfProperty = allProperties.indexOf(property);
+      allProperties.splice(indexOfProperty, 1);
+    });
+
     // create property entry
     const propertyDetails = document.createElement("section");
     propertyDetails.textContent = `Street Address: ${property.address}, Property Type: ${property.property}, City: ${property.city}, State: ${property.state}`;
     // append property details and delete button to section
-    propertyElement.append(propertyDetails, delButton);
+    propertyElement.append(propertyDetails, delButton, commentButton);
     // return section
     return propertyElement;
   });
+  // replace all children with new updated array
   properties.replaceChildren(...propertyElArray);
 });
